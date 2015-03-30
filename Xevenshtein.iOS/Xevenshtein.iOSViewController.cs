75e07724d89268475ceb2746dcd651304cfaccc1
Antonio Feregrino <antonio.feregrino@gmail.com>
2015-03-30 17:02:31 -0600

    iOS project now works

---
+++ Xevenshtein.iOS/Xevenshtein.iOSViewController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+
+using Foundation;
+using UIKit;
+
+namespace Xevenshtein.iOS
+{
+	public partial class Xevenshtein_iOSViewController : UIViewController
+	{
+		public Xevenshtein_iOSViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		#region View lifecycle
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			ComputeButton.TouchUpInside += (sender, e) => { 
+				var firsWord = FirstWordInputText.Text;
+				var secondWord = SecondWordInputText.Text;
+				var compute = Xevenshtein.Core.Levenshtein.Compute(firsWord, secondWord);
+				ResultLabel.Text = compute.ToString();
+			};
+
+		}
+
+		public override void ViewWillAppear (bool animated)
+		{
+			base.ViewWillAppear (animated);
+		}
+
+		public override void ViewDidAppear (bool animated)
+		{
+			base.ViewDidAppear (animated);
+		}
+
+		public override void ViewWillDisappear (bool animated)
+		{
+			base.ViewWillDisappear (animated);
+		}
+
+		public override void ViewDidDisappear (bool animated)
+		{
+			base.ViewDidDisappear (animated);
+		}
+
+		#endregion
+	}
+}
+
